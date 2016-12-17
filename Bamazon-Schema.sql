94ab21ee79e2e877524473fe97ff35641c08cc6e
Lori Dowers <ldowers@cfl.rr.com>
2016-12-17 02:08:50 -0500

    Updated SQL Scripts

--- Bamazon-Schema.sql
+++ Bamazon-Schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE products
 item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
 product_name VARCHAR(100) NULL,
 department_name VARCHAR(100) NULL,
-price FLOAT(10) NULL,
+price DECIMAL(10,2) NULL,
 stock_quantity INTEGER(10) NULL,
 PRIMARY KEY (item_id)
 );
